69221fa58aa022262c09fa9987d78bfb772e7449
andresdiazalvarez <ingenierotecni3@gmail.com>
2026-06-30 20:39:26 +0200

    Add files via upload

--- seed-data.js
+++ seed-data.js
@@ -1,15 +1,1 @@
-{
-  "name": "Extintor",
-  "short_name": "Extintor",
-  "start_url": "./",
-  "display": "standalone",
-  "background_color": "#f3f0eb",
-  "theme_color": "#facc15",
-  "icons": [
-    {
-      "src": "icon.svg",
-      "sizes": "any",
-      "type": "image/svg+xml"
-    }
-  ]
-}
+window.INITIAL_EXTINTORES_LISTADOS = [];
