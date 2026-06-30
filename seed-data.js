4924cf870e8a483a21e62bf173f25cc6741b74b3
andresdiazalvarez <ingenierotecni3@gmail.com>
2026-06-30 19:36:30 +0200

    Add files via upload

---
+++ seed-data.js
@@ -0,0 +1,15 @@
+{
+  "name": "Extintor",
+  "short_name": "Extintor",
+  "start_url": "./",
+  "display": "standalone",
+  "background_color": "#f3f0eb",
+  "theme_color": "#b91c1c",
+  "icons": [
+    {
+      "src": "icon.svg",
+      "sizes": "any",
+      "type": "image/svg+xml"
+    }
+  ]
+}
